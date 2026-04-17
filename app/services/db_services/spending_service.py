e7200e3ad88d320a3bf6ca2ecb6621fa350f2e11
Shihui Lu <shihui.lu51@gmail.com>
2026-04-16 23:14:14 -0400

    Exclude filters with none as value

--- app/services/db_services/spending_service.py
+++ app/services/db_services/spending_service.py
@@ -7,20 +7,6 @@ from app.models.spending_model import SpendingModel
 from app.schemas.spending_schema import SpendingInsertSchema, SpendingSelectSchema
 
 
-def select_spendings(
-    filters: SpendingSelectSchema, db: Session
-) -> Sequence[SpendingModel]:
-    stmt = select(SpendingModel).filter_by(**filters.model_dump())
-    result = db.scalars(stmt).all()
-    return result
-
-
-def select_all_spendings(db: Session) -> Sequence[SpendingModel]:
-    stmt = select(SpendingModel)
-    result = db.scalars(stmt).all()
-    return result
-
-
 def insert_spendings(
     spendings_data: list[SpendingInsertSchema], db: Session
 ) -> list[int]:
@@ -29,3 +15,17 @@ def insert_spendings(
     db.flush()
     spending_ids = [spending.spending_id for spending in spendings]
     return spending_ids
+
+
+def select_spendings(
+    filters: SpendingSelectSchema, db: Session
+) -> Sequence[SpendingModel]:
+    stmt = select(SpendingModel).filter_by(**filters.model_dump(exclude_none=True))
+    result = db.scalars(stmt).all()
+    return result
+
+
+def select_all_spendings(db: Session) -> Sequence[SpendingModel]:
+    stmt = select(SpendingModel)
+    result = db.scalars(stmt).all()
+    return result
